8a558c8859b5f71b97693481f926de7e3348f767
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-04 05:59:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,12 +84,11 @@ def adding_matz
         }
      }
 
-		 programmer_hash << {
+		 puts programmer_hash
 	 		:yukihiro_matsumoto => {
 	 		   :known_for => "Ruby",
 	 		   :languages => ["LISP", "C"]
 	 	}
-	}
 end
 
 def changing_alan
